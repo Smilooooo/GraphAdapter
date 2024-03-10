c91ec227f6a7ae5160c0d038bfe1f7e37a8c465a
Xin Li <92313416+lixinustc@users.noreply.github.com>
2024-03-10 14:19:35 +0800

    Add files via upload

---
+++ scripts/graphclip/main.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# custom config
+DATA=/data/datasets/
+TRAINER=GraphCLIP_v1
+
+DATASET=$1
+CFG=$2  # config file
+INIT_WEIGHTS=$3
+CTP=$4  # class token position (end or middle)
+NCTX=$5  # number of context tokens
+SHOTS=$6  # number of shots (1, 2, 4, 8, 16)
+CSC=$7  # class-specific context (False or True)
+SEE=$8
+for SEED in ${SEE}
+do
+    DIR=output/${DATASET}/${TRAINER}/${CFG}_${SHOTS}shots/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}
+    if [ -d "$DIR" ]; then
+        echo "Oops! The results exist at ${DIR} (so skip this job)"
+    else
+        python train.py \
+        --root ${DATA} \
+        --seed ${SEED} \
+        --init_weights ${INIT_WEIGHTS} \
+        --trainer ${TRAINER} \
+        --dataset-config-file configs/datasets/${DATASET}.yaml \
+        --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+        --output-dir ${DIR} \
+        TRAINER.COOP.N_CTX ${NCTX} \
+        TRAINER.COOP.CSC ${CSC} \
+        TRAINER.COOP.CLASS_TOKEN_POSITION ${CTP} \
+        DATASET.NUM_SHOTS ${SHOTS}
+    fi
+done
